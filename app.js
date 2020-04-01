ecb2b6a7d2b1ad521156c5220ff1328876643707
Leonardo Mazza <42423607+LeonardoMazza@users.noreply.github.com>
2020-04-01 18:19:29 -0300

    Exibir valores na tela
    Exibindo os valores cadastrados no banco de dados na index

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express    = require('express');
 const app        = express();
+const path       = require('path');
+const exphbs     = require('express-handlebars');
 const db         = require('./db/connection');
 const bodyParser = require('body-parser');
+const Job        = require('./models/Job');
 
 const PORT = 3000;
 
@@ -12,6 +15,14 @@ app.listen(PORT, function(){
 // body parser
 
 app.use(bodyParser.urlencoded({extended:false}));
+
+// handle bars //
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Conexão com o Banco de Dados
 db
@@ -25,7 +36,16 @@ db
 
 // Rotas
 app.get('/', (req, res) => {
-    res.send("Está funcionando");
+    Job.findAll({order: [
+        ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+        res.render('index', {
+            jobs
+        });
+         
+    });
+
 });
 
 // Rotas Jobs
