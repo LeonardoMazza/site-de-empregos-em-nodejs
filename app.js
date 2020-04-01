e37371b07ae2c6a65720b32475cccae973128a46
Leonardo Mazza <42423607+LeonardoMazza@users.noreply.github.com>
2020-04-01 20:54:10 -0300

    Exibição de Vagas
    Criação do método e ligação com o front para exibir as vagas de emprego.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express    = require('express');
+const exphbs     = require('express-handlebars');
 const app        = express();
 const path       = require('path');
-const exphbs     = require('express-handlebars');
 const db         = require('./db/connection');
 const bodyParser = require('body-parser');
 const Job        = require('./models/Job');
@@ -10,69 +10,69 @@ const Op         = Sequelize.Op;
 
 const PORT = 3000;
 
-app.listen(PORT, function(){
-    console.log(`O express está rodando na porta ${PORT}`);
+app.listen(PORT, function() {
+  console.log(`O Express está rodando na porta ${PORT}`);
 });
 
 // body parser
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// handle bars //
+// handle bars
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-//static folder
+// static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Conexão com o Banco de Dados
+// db connection
 db
-    .authenticate()
-    .then(() => {
-        console.log("Conectou ao banco com sucesso");
-    })
-    .catch(err => {
-        console.log("Ocorreu um erro ao conectar", err);
-    });
+  .authenticate()
+  .then(() => {
+    console.log("Conectou ao banco com sucesso");
+  })
+  .catch(err => {
+    console.log("Ocorreu um erro ao conectar", err);
+  });
 
-// Rotas
+// routes
 app.get('/', (req, res) => {
 
-    let search = req.query.job;
-    let query = '%'+search+'%'; // PH -> PHP, Word -> WordPress
+  let search = req.query.job;
+  let query  = '%'+search+'%'; // PH -> PHP, Word -> Wordpress, press -> Wordpress
 
-    if(!search) {
-        Job.findAll({order: [
-            ['createdAt', 'DESC']
-        ]})
-        .then(jobs => {
+  if(!search) {
+    Job.findAll({order: [
+      ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+  
+      res.render('index', {
+        jobs
+      });
+  
+    })
+    .catch(err => console.log(err));
+  } else {
+    Job.findAll({
+      where: {title: {[Op.like]: query}},
+      order: [
+        ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+      console.log(search);
+      console.log(search);
+  
+      res.render('index', {
+        jobs, search
+      });
+  
+    })
+    .catch(err => console.log(err));
+  }
 
-            res.render('index', {
-                jobs
-            });
-             
-        })
-        .catch(err => console.log(err));
-    } else {
-        Job.findAll({
-            where: {title: {[Op.like]: query}},
-            order: [
-            ['createdAt', 'DESC']
-        ]})
-        .then(jobs => {
-            console.log(search);
-            console.log(search);
-            
-            res.render('index', {
-                jobs, search
-            });
-             
-        })
-        .catch(err => console.log(err));
-    }
-
-
+  
 });
 
-// Rotas Jobs
+// jobs routes
 app.use('/jobs', require('./routes/jobs'));
