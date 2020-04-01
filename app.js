6b9cc73c638ef0b88ff43fb480876310409bbbee
Leonardo Mazza <42423607+LeonardoMazza@users.noreply.github.com>
2020-04-01 19:32:38 -0300

    Finalização da busca pelo formulário
    Finalização da integração do back-end com o front-end na busca do formulário.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const exphbs     = require('express-handlebars');
 const db         = require('./db/connection');
 const bodyParser = require('body-parser');
 const Job        = require('./models/Job');
+const Sequelize  = require('sequelize');
+const Op         = Sequelize.Op;
 
 const PORT = 3000;
 
@@ -13,7 +15,6 @@ app.listen(PORT, function(){
 });
 
 // body parser
-
 app.use(bodyParser.urlencoded({extended:false}));
 
 // handle bars //
@@ -36,15 +37,40 @@ db
 
 // Rotas
 app.get('/', (req, res) => {
-    Job.findAll({order: [
-        ['createdAt', 'DESC']
-    ]})
-    .then(jobs => {
-        res.render('index', {
-            jobs
-        });
-         
-    });
+
+    let search = req.query.job;
+    let query = '%'+search+'%'; // PH -> PHP, Word -> WordPress
+
+    if(!search) {
+        Job.findAll({order: [
+            ['createdAt', 'DESC']
+        ]})
+        .then(jobs => {
+
+            res.render('index', {
+                jobs
+            });
+             
+        })
+        .catch(err => console.log(err));
+    } else {
+        Job.findAll({
+            where: {title: {[Op.like]: query}},
+            order: [
+            ['createdAt', 'DESC']
+        ]})
+        .then(jobs => {
+            console.log(search);
+            console.log(search);
+            
+            res.render('index', {
+                jobs, search
+            });
+             
+        })
+        .catch(err => console.log(err));
+    }
+
 
 });
 
